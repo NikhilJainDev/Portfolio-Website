1caa662cfc8634d2fd965565985f311caee2afc4
Nikhil jain <nikhiljain90598@gmail.com>
2025-07-23 17:22:56 +0530

    Initial commit of fullstack portfolio

--- client/src/components/Menus/Menus.js
+++ client/src/components/Menus/Menus.js
@@ -1,137 +1,136 @@
-import React from 'react'
+import React from 'react';
 import './Menu.css';
-import Zoom from 'react-reveal/Zoom';
-import Fade from 'react-reveal/Fade';
-import {FcAbout, FcBiotech, FcBusinessContact, FcGraduationCap, FcHome, FcPortraitMode, FcVideoProjector, FcVoicePresentation} from 'react-icons/fc';
+import { motion } from 'framer-motion';
+import {
+  FcAbout,
+  FcBiotech,
+  FcBusinessContact,
+  FcGraduationCap,
+  FcHome,
+  FcPortraitMode,
+  FcVideoProjector,
+  FcVoicePresentation
+} from 'react-icons/fc';
 import { Link } from 'react-scroll';
-const Menus = ({toggle}) => {
+
+const Menus = ({ toggle }) => {
   return (
-   <>
-
-   {toggle ? (
     <>
-    <Zoom>
-    <div className="navbar-profile-pic">
-        <img
-        src="https://cdn-icons-png.flaticon.com/512/5726/5726600.png"
-        alt="Profile Picture"
-        
-        />
-    </div>
-    </Zoom>
-    <Fade left>
-    <div className="nav-items">
-    <div className="nav-item">
-            <div className="nav-link">
-              <Link to="home" spy={true} smooth={true} offset={-100} duration={100}>
-              <FcHome/>
-                Home
-              </Link>
-                </div>
+      {toggle ? (
+        <>
+          {/* Profile Picture with Zoom Effect */}
+          <motion.div
+            className="navbar-profile-pic"
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            <img
+  src="https://cdn-icons-png.flaticon.com/512/5726/5726600.png"
+  alt="Nikhil Jain"
+/>
+          </motion.div>
 
-                <div className="nav-link">
-               <Link to="about" spy={true} smooth={true} offset={-100} duration={100}>
-               <FcAbout/>
-               About
-               </Link>
-                </div>
+          {/* Side Nav Items with Slide In Left */}
+          <motion.div
+            className="nav-items"
+            initial={{ x: -100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.7 }}
+          >
+            <div className="nav-item">
+              <div className="nav-link">
+                <Link to="home" spy={true} smooth={true} offset={-100} duration={100}>
+                  <FcHome />
+                  Home
+                </Link>
+              </div>
 
+              <div className="nav-link">
+                <Link to="about" spy={true} smooth={true} offset={-100} duration={100}>
+                  <FcAbout />
+                  About
+                </Link>
+              </div>
 
-                <div className="nav-link">
-               <Link to="education" spy={true} smooth={true} offset={-100} duration={100}>
-               <FcGraduationCap/>
-               Education
-               </Link>
-                </div>
+              <div className="nav-link">
+                <Link to="education" spy={true} smooth={true} offset={-100} duration={100}>
+                  <FcGraduationCap />
+                  Education
+                </Link>
+              </div>
 
-                <div className="nav-link">
+              <div className="nav-link">
                 <Link to="techstack" spy={true} smooth={true} offset={-100} duration={100}>
-                <FcBiotech/>
-                Tech Stack
+                  <FcBiotech />
+                  Tech Stack
                 </Link>
-                </div>
+              </div>
 
-                <div className="nav-link">
-                <Link  to="projects" spy={true} smooth={true} offset={-100} duration={100}>
-                <FcVideoProjector/>
-                Projects
+              <div className="nav-link">
+                <Link to="projects" spy={true} smooth={true} offset={-100} duration={100}>
+                  <FcVideoProjector />
+                  Projects
                 </Link>
-                </div>
+              </div>
 
-                 <div className="nav-link">
-               <Link to="work" spy={true} smooth={true} offset={-100} duration={100}>
-               <FcPortraitMode/>
-               Work Experience
-               </Link>
-                </div>
+              <div className="nav-link">
+                <Link to="work" spy={true} smooth={true} offset={-100} duration={100}>
+                  <FcPortraitMode />
+                  Work Experience
+                </Link>
+              </div>
 
-                <div className="nav-link">
+              <div className="nav-link">
                 <Link to="contacts" spy={true} smooth={true} offset={-100} duration={100}>
-                <FcBusinessContact/>
-                Contacts
+                  <FcBusinessContact />
+                  Contacts
                 </Link>
-                </div>
-        </div>
-    </div>
-    </Fade>
+              </div>
+            </div>
+          </motion.div>
+        </>
+      ) : (
+        <>
+          <div className="nav-items">
+            <div className="nav-item">
+              <div className="nav-link">
+                <FcHome title="Home" />
+              </div>
+
+              <div className="nav-link">
+                <FcAbout title="About" />
+              </div>
+
+              <div className="nav-link">
+                <FcPortraitMode title="Experience" />
+              </div>
+
+              <div className="nav-link">
+                <FcBiotech title="Technology" />
+              </div>
+
+              <div className="nav-link">
+                <FcGraduationCap title="Education" />
+              </div>
+
+              <div className="nav-link">
+                <FcVideoProjector title="Projects" />
+              </div>
+
+              <div className="nav-link">
+                <FcVoicePresentation title="Testimonial" />
+              </div>
+
+              <div className="nav-link">
+                <FcBusinessContact title="Contacts" />
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </>
-
-   ) : (
-    <>
-
-    // Same Comment 
-
-    <div className="nav-items">
-    <div className="nav-item">
-            <div className="nav-link">
-                <FcHome title='Home'/>
-                
-                </div>
-
-                <div className="nav-link">
-                <FcAbout title='About'/>
-                
-                </div>
-
-                <div className="nav-link">
-                <FcPortraitMode title='Experience'/>
-                
-                </div>
-
-                <div className="nav-link">
-                <FcBiotech title='Technology'/>
-               
-                </div>
-
-                <div className="nav-link">
-                <FcGraduationCap title='Education'/>
-                
-                </div>
-
-                <div className="nav-link">
-                <FcVideoProjector title='Projects'/>
-                
-                </div>
-
-                <div className="nav-link">
-                <FcVoicePresentation title='TestiMonial'/>
-                
-                </div>
-
-                <div className="nav-link">
-                <FcBusinessContact title='Contacts'/>
-               
-                </div>
-        </div>
-    </div>
-
-
-    </>
-  
-   )}
-   <Fade />
-    </>
-   
   );
 };
+
 export default Menus;
--- server.js
+++ server.js
@@ -1,22 +1,24 @@
-const express =  require('express');
-const cors =  require('cors');
-const dotenv =  require('dotenv');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
-// Rest Objects
-const app = express()
- 
-// dotenv configraution 
-dotenv.config()
-// middleWares 
-app.use(cors()) 
-app.use(express.json())
-// routes
-app.use('/api/v1/portfolio', require('./routes/portfolioRoute'))
-// Ports
-const PORT = process.env.PORT || 8080
+//dotenv configuartion
+dotenv.config();
+
+//rest object
+const app = express();
+
+//midlewares
+app.use(cors());
+app.use(express.json());
+
+//routes
+app.use("/api/v1/portfolio", require("./routes/portfolioRoute"));
+
+//port
+const PORT = process.env.PORT || 8080;
+
 //listen
-app.listen(PORT, ()=>{
-    console.log( `Server Running on Port ${PORT} `)
-})
-
-
+app.listen(PORT, () => {
+  console.log(`Server Runnning On PORT ${PORT} `);
+});
